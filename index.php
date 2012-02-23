bf61f853cc2102aa75d6758c4e9b7237e1d4d5a9
robin <robinwageman@gmail.com>
2012-02-23 10:14:40 +0100

    new files for pagination

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php 
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+require('paginatorclass.php');
+
+$dbcon = mysql_connect("195.20.9.80", "dfguser", "dfg21sh");
+mysql_select_db("dfguser", $dbcon) or trigger_error(mysql_error());
+
+$query = "SELECT id FROM gastenboek";
+$showquery = "SELECT * FROM gastenboek";
+$berichtperpagina = 2;
+		
+$pagination = new Paginator($query,$showquery,$berichtperpagina);
+
+$geenberichten = "er zijn geen berichten";
+$berichten = $pagination->Showall($geenberichten);
+if (is_array($berichten)){
+	foreach($berichten as $result){
+		echo $result['naam']."<br />";
+		echo $result['titel']."<br />";
+		echo $result['bericht']."<br />";
+		echo $result['plaats_datum']."<br />";
+	}
+}
+
+$weergavefirst = '<--'; 
+$weergaveprev = '<-'; 
+$weergavenext = '->'; 
+$weergavelast = '-->'; 
+echo $pagination->Firstpage($weergavefirst);
+echo $pagination->Previouspage($weergaveprev);
+echo $pagination->Nextpage($weergavenext);
+echo $pagination->Lastpage($weergavelast);
+echo '<br />';
+echo $pagination->Pagenumbers(3);
+
+
+?>
+
