4ea7f689eeae51c0f364324079478153f1a5acad
robin <robinwageman@gmail.com>
2012-03-06 15:53:48 +0100

    phpunit rekenmasjiene

--- phpunit.php
+++ phpunit.php
@@ -13,4 +13,51 @@ class PhpUnitTest extends PHPUnit_Framework_TestCase
     {
     }
 }
+
+class RekenMaarRaak extends PHPUnit_Framework_TestCase
+{
+	public $getal1;
+	public $getal2;
+	public $getal3;
+
+	public function add($getal1, $getal2)
+	{
+		$this->getal1 = 5;
+		$this->getal2 = 3;
+		$this->getal1 + $this->getal2 = $this->getal3;
+		$this->assertEquals($this->getal3, 8);
+		
+		return $this->getal3;
+	}	
+	public function subtract()
+	{
+		$this->getal1 = 5;
+		$this->getal2 = 3;
+		$this->getal1 - $this->getal2 = $this->getal3;
+		$this->assertEquals($this->getal3, 2);
+
+		return $this->getal3;
+	}
+
+	public function multiply()
+	{
+		$this->getal1 = 5;
+		$this->getal2 = 3;
+		$this->getal1 * $this->getal2 = $this->getal3;
+		$this->assertEquals($this->getal3, 15);
+
+		return $this->getal3;
+	}
+	public function divide()
+	{
+		$this->getal1 = 5;
+		$this->getal2 = 3;
+		$this->getal1 / $this->getal2 = $this->getal3;
+		$this->assertEquals($this->getal3, 25);
+
+		return $this->getal3;
+	}
+}
+
+
 ?>
